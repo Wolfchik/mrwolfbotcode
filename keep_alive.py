d295b52e4cca1e2cff208b225c088a9aa22c5e83
Wolfchik <88947797+Wolfchik@users.noreply.github.com>
2022-02-02 00:56:28 +0300

    Update keep_alive.py
    Run Hosting

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('app')
 
 @app.route('/')
 def home():
-    return "HTML CODE returned error: На этом сайте ничего нет."
+    return "HTML CODE returned error: На этом сайте ничего нет"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
